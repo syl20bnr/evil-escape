c07bb6e95fdd291fded6282d211f60c7e3b8efec
sbenner <sylvain.benner@ubisoft.com>
2015-03-31 08:14:07 -0400

    Bump to version 2.18

--- evil-escape.el
+++ evil-escape.el
@@ -5,7 +5,7 @@
 ;; Author: Sylvain Benner <sylvain.benner@gmail.com>
 ;; Keywords: convenience editing evil
 ;; Created: 22 Oct 2014
-;; Version: 2.17
+;; Version: 2.18
 ;; Package-Requires: ((emacs "24") (evil "1.0.9"))
 ;; URL: https://github.com/syl20bnr/evil-escape
 
