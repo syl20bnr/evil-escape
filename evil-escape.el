ddb2348b40482d38e6e4836bd083f3e674cd9cd8
syl20bnr <sylvain.benner@gmail.com>
2015-09-02 00:59:06 -0400

    Bump version to 3.02

--- evil-escape.el
+++ evil-escape.el
@@ -5,7 +5,7 @@
 ;; Author: Sylvain Benner <sylvain.benner@gmail.com>
 ;; Keywords: convenience editing evil
 ;; Created: 22 Oct 2014
-;; Version: 3.01
+;; Version: 3.02
 ;; Package-Requires: ((emacs "24") (evil "1.0.9"))
 ;; URL: https://github.com/syl20bnr/evil-escape
 
