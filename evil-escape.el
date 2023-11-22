04b6e9c0cf34f949d15d82c7bbc0f405fffbd5df
Jonas Bernoulli <jonas@bernoul.li>
2023-11-22 19:56:19 +0100

    Depend on latest Evil release

--- evil-escape.el
+++ evil-escape.el
@@ -6,7 +6,7 @@
 ;; Keywords: convenience editing evil
 ;; Created: 22 Oct 2014
 ;; Version: 3.16
-;; Package-Requires: ((emacs "24") (evil "1.0.9") (cl-lib "0.5"))
+;; Package-Requires: ((emacs "24") (evil "1.14.0") (cl-lib "0.5"))
 ;; URL: https://github.com/emacsorphanage/evil-escape
 
 ;; This file is not part of GNU Emacs.
